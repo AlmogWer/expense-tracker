f4db9fce43153f340da37780be9909ab4525c3d7
Almog Wertzberger <Almogish@gmail.com>
2021-08-25 18:47:37 +0300

    adding functionality

---
+++ README.md
@@ -0,0 +1,26 @@
+<h1 align="center">Expense tracker</h1>
+
+## Project Description
+
+Keep track of income and expenses. Add and remove items and save to local storage.
+
+## Built with
+
+- HTML
+- CSS
+- JavaScript
+
+## Links
+
+- [Repo](https://github.com/AlmogWer/expense-tracker "Expense-tracker Repo")
+- [Live](https://almogwer.github.io/expense-tracker/ "Live View")
+
+## Screenshots
+
+![](img/Capture.PNG "Home Page")
+
+## Author
+
+- [Almogwer](https://github.com/almogwer)
+- [Mail](mailto:Almogish@gmail.com?Subject=Hi% "Hi!")
+- [Linkedin](https://www.linkedin.com/in/almogwertzberger/)
--- script.js
+++ script.js
@@ -5,16 +5,49 @@ const list = document.getElementById("list");
 const form = document.getElementById("form");
 const text = document.getElementById("text");
 const amount = document.getElementById("amount");
+const closeBtn = document.getElementById("notice-button");
+const alert = document.getElementById("notice");
 
-const dummyTransactions = [
-  { id: 1, text: "Flower", amount: -20 },
-  { id: 2, text: "Salary", amount: 300 },
-  { id: 3, text: "Book", amount: -10 },
-  { id: 4, text: "Camera", amount: 150 },
-];
+// const dummyTransactions = [
+//   { id: 1, text: "Flower", amount: -20 },
+//   { id: 2, text: "Salary", amount: 300 },
+//   { id: 3, text: "Book", amount: -10 },
+//   { id: 4, text: "Camera", amount: 150 },
+// ];
 
-let transactions = dummyTransactions;
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem("transactions")
+);
 
+let transactions =
+  localStorage.getItem("transactions") !== null ? localStorageTransactions : [];
+
+//Add transaction
+function addTransaction(e) {
+  e.preventDefault();
+  if (text.value.trim() === "" || amount.value.trim() === "") {
+    alert.classList.add("show");
+
+    alert("please add a text and amount");
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: text.value.charAt(0).toUpperCase() + text.value.slice(1),
+      amount: +amount.value,
+    };
+    transactions.push(transaction);
+    addTransactionDOM(transaction);
+    updateValues();
+    updateLocalStorage();
+    text.value = "";
+    amount.value = "";
+  }
+}
+
+//Generate random ID
+function generateID() {
+  return Math.floor(Math.random() * 1000000);
+}
 //Add transactions to DOM list
 function addTransactionDOM(transaction) {
   //Get sign
@@ -25,13 +58,62 @@ function addTransactionDOM(transaction) {
   item.innerHTML = `
   ${transaction.text}<span>${sign}${Math.abs(
     transaction.amount
-  )}</span><button class="delete-btn">X</button>
+  )}</span><button class="delete-btn"onclick="removeTransaction(${
+    transaction.id
+  })">X</button>
   `;
   list.appendChild(item);
+}
+
+//Update the balance, income and expense
+function updateValues() {
+  const amounts = transactions.map((transaction) => transaction.amount);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+  balance.innerText = `$${total}`;
+  money_plus.innerText = `$${income}`;
+  money_minus.innerText = `$${expense}`;
+}
+
+//Remove transaction by ID
+function removeTransaction(id) {
+  transactions = transactions.filter((transaction) => transaction.id !== id);
+
+  updateLocalStorage();
+
+  init();
+}
+
+//Update local storage transactions
+function updateLocalStorage() {
+  localStorage.setItem("transactions", JSON.stringify(transactions));
 }
 //Init app
 function init() {
   list.innerHTML = "";
   transactions.forEach(addTransactionDOM);
+  updateValues();
 }
+
 init();
+
+//Event listeners
+form.addEventListener("submit", addTransaction);
+
+//Popup functionality
+closeBtn.addEventListener("click", function () {
+  alert.classList.remove("show");
+});
+
+const popups = [...document.getElementsByClassName("notice")];
+
+window.addEventListener("click", (e) =>
+  e.target == alert ? alert.classList.remove("show") : false
+);
